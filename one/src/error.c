4c6a358d54467c7900cafc7ac3bbd6e42bd1475f
Sean McBride <brennsto@gmail.com>
2024-12-26 19:39:02 -0800

    day one solved

---
+++ one/src/error.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "error.h"
+
+void fatal_err(char* msg) {
+    fprintf(stderr, "FATAL ERROR: %s\n", msg);
+    exit(1);
+}
+
+void check_malloc(void* ptr) {
+    if(ptr == NULL){
+        fprintf(stderr, "Out of memory\n");
+        exit(1);
+    }
+}
