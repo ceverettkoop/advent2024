f4c508edbb338a10f82d3a34b88d5e0f7e24875b
Sean McBride <brennsto@gmail.com>
2024-12-27 19:54:28 -0800

    part two done

--- three/src/main.c
+++ three/src/main.c
@@ -11,7 +11,6 @@
 
 static int extract_product(const fpos_t file_pos, FILE *fp) {
     // assume provided fp is at pos
-
     int c = 0;
     int result = 0;
     char int_token_a[TOKEN_MAX] = {'\0'};
@@ -50,8 +49,6 @@ static int extract_product(const fpos_t file_pos, FILE *fp) {
     }
     // presumably two valid numbers
     result = atoi(int_token_a) * atoi(int_token_b);
-    //printf("mutiplying token %s and token %s to get result: %d\n", int_token_a, int_token_b, result);
-
 EXIT:
     fsetpos(fp, &file_pos);
     if (result == 0) printf("note we found an invalid token starting with mul at position %lld\n", file_pos);
@@ -62,10 +59,17 @@ int main(int argc, char const *argv[]) {
     const char path[] = "./input";
     int c = 0;
     const char begin_token[] = "mul(";
+    const char do_token[] = "do()";
+    const char dont_token[] = "don't()";
     size_t begin_token_len = strlen(begin_token);
-    int search_index = 0;
+    size_t do_token_len = strlen(do_token);
+    size_t dont_token_len = strlen(dont_token);
+    int mul_token_index = 0;
+    int do_token_index = 0;
+    int dont_token_index = 0;
     fpos_t pos = 0;
     unsigned result = 0;
+    bool parsing_on = true;
 
     // open input
     FILE *fp = fopen(path, "r");
@@ -75,17 +79,38 @@ int main(int argc, char const *argv[]) {
     while (c != EOF) {
         c = fgetc(fp);
         pos++;
-        if (c == begin_token[search_index]) {
-            search_index++;
+        // looking for mul
+        if (c == begin_token[mul_token_index]) {
+            mul_token_index++;
             // success, pos now = index of end
-            if (search_index == begin_token_len) {
+            if (mul_token_index == begin_token_len) {
                 int product = extract_product(pos, fp);
                 // returns 0 on failure, resets fp either way
-                result += product;
-                search_index = 0;
+                if (parsing_on) result += product;
+                mul_token_index = 0;
             }
         } else {
-            search_index = 0;
+            mul_token_index = 0;
+        }
+        // looking for do
+        if (c == do_token[do_token_index]) {
+            do_token_index++;
+            if (do_token_index == do_token_len) {
+                parsing_on = true;
+                do_token_index = 0;
+            }
+        } else {
+            do_token_index = 0;
+        }
+        //looking for dont
+        if (c == dont_token[dont_token_index]) {
+            dont_token_index++;
+            if (dont_token_index == dont_token_len) {
+                parsing_on = false;
+                dont_token_index = 0;
+            }
+        } else {
+            dont_token_index = 0;
         }
     }
 
