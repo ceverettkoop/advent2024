f720f324c3893271d0f91aeb2e55e2978dd716d1
Sean <brennsto@gmail.com>
2024-12-30 14:08:11 -0800

    part two done real nested loop hours

--- five/src/main.c
+++ five/src/main.c
@@ -12,73 +12,82 @@
 #define MAX_DIGITS 8
 #define MAX_UPDATE_PAGES 32
 
-const char path[] = "./dummyinput";
+const char path[] = "./input";
 
-typedef struct Pair_tag{
-    int before;
-    int after;
-}Pair;
+typedef struct Pair_tag {
+        int before;
+        int after;
+} Pair;
 
-static bool is_out_of_order(int precedent, int antecedent, Pair *pairs, size_t pair_ct){
-    for (size_t i = 0; i < pair_ct; i++){
+static bool is_out_of_order(int precedent, int antecedent, Pair *pairs, size_t pair_ct) {
+    for (size_t i = 0; i < pair_ct; i++) {
         Pair pair = pairs[i];
-        if(precedent == pair.after && antecedent == pair.before) return true;
+        if (precedent == pair.after && antecedent == pair.before) return true;
     }
     return false;
 }
 
-static void revise_update(int *input, Pair *pairs, size_t pair_ct){
+static void revise_update(int *input, Pair *pairs, size_t pair_ct) {
     size_t update_size = 0;
     int value = 0;
-    while(value != -1){
+    while (value != -1) {
         update_size++;
         value = input[update_size];
     }
-
-    for (int i = (update_size - 1); i > -1; i--){
+    for (int i = (update_size - 1); i > -1; i--) {
         int antecedent = input[i];
-        for (int j = i - 1; j > -1; j--){
+        // inner loop, compare everything before antecedent
+        for (int j = i - 1; j > -1; j--) {
             int precedent = input[j];
-            if(is_out_of_order(precedent, antecedent, pairs, pair_ct)){
-                //swapping them will work as long as each value appears in one pair only
-                input[i] = precedent;
-                input[j] = antecedent;
+            // iterate through every rule
+            for (size_t k = 0; k < pair_ct; k++) {
+                Pair pair = pairs[k];
+                if (precedent == pair.after && antecedent == pair.before) {
+                    // on every rule violation, swap them
+                    input[j] = antecedent;
+                    input[i] = precedent;
+                    // now what we are comparing changes as well
+                    int temp_a = antecedent;
+                    int temp_p = precedent;
+                    precedent = temp_a;
+                    antecedent = temp_p;
+                }
             }
         }
     }
 }
 
-static int get_middle_value(int* update){
+static int get_middle_value(int *update) {
     size_t update_size = 0;
     int value = 0;
 
-    while(value != -1){
+    while (value != -1) {
         update_size++;
         value = update[update_size];
     }
     return update[update_size / 2];
 }
 
-static bool is_valid_update(int *update, Pair *pairs, size_t pair_ct){
+static bool is_valid_update(int *update, Pair *pairs, size_t pair_ct) {
     size_t update_size = 0;
     int value = 0;
 
-    while(value != -1){
+    while (value != -1) {
         update_size++;
         value = update[update_size];
     }
-    //start with last value, work backwards
-    for (int i = (update_size - 1); i > -1; i--){
+    // start with last value, work backwards
+    for (int i = (update_size - 1); i > -1; i--) {
         int antecedent = update[i];
-        for (int j = i - 1; j > -1; j--){
+        for (int j = i - 1; j > -1; j--) {
             int precedent = update[j];
-            if(is_out_of_order(precedent, antecedent, pairs, pair_ct)) return false;
+            if (is_out_of_order(precedent, antecedent, pairs, pair_ct)) return false;
         }
-     }
-     return true;
+    }
+    return true;
 }
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[]) {
     int c = 0;
     int last_c = 0;
     char input_buffer[MAX_DIGITS] = {'\0'};
@@ -90,8 +99,8 @@ int main(int argc, char const *argv[]){
     size_t pair_ct = 0;
     unsigned part_one_result = 0;
     unsigned part_two_result = 0;
-    
-    //idg why this doesn't work, it is setting them to 0 not -1 but whatever
+
+    // idg why this doesn't work, it is setting them to 0 not -1 but whatever
     Pair pairs[MAX_PAIRS] = {(Pair){.before = -1, .after = -1}};
     int updates[MAX_UPDATES][MAX_UPDATE_PAGES] = {{-1}};
 
@@ -99,24 +108,24 @@ int main(int argc, char const *argv[]){
     FILE *fp = fopen(path, "r");
     if (fp == NULL) fatal_err("failed to open file\n");
 
-    //parse part one
-    while(true){
+    // parse part one
+    while (true) {
         last_c = c;
         c = fgetc(fp);
-        if(c == '\n' && last_c == '\n') break; //end part one
-        if(isdigit(c)){
+        if (c == '\n' && last_c == '\n') break;  // end part one
+        if (isdigit(c)) {
             input_buffer[buffer_index] = c;
             buffer_index++;
             continue;
-        } 
-        if(c == '|'){
-            pairs[pair_index].before = atoi(input_buffer); 
+        }
+        if (c == '|') {
+            pairs[pair_index].before = atoi(input_buffer);
             buffer_index = 0;
             memset(input_buffer, '\0', MAX_DIGITS);
             continue;
         }
-        if(c == '\n'){
-            pairs[pair_index].after = atoi(input_buffer); 
+        if (c == '\n') {
+            pairs[pair_index].after = atoi(input_buffer);
             pair_index++;
             buffer_index = 0;
             memset(input_buffer, '\0', MAX_DIGITS);
@@ -125,38 +134,38 @@ int main(int argc, char const *argv[]){
     }
     pair_ct = pair_index;
 
-    //parse part two
-    while(c != EOF){
+    // parse part two
+    while (c != EOF) {
         c = fgetc(fp);
-        if(isdigit(c)){
+        if (isdigit(c)) {
             input_buffer[buffer_index] = c;
             buffer_index++;
             continue;
         }
-        if(c == ','){
+        if (c == ',') {
             updates[update_index][page_index] = atoi(input_buffer);
             buffer_index = 0;
             page_index++;
             continue;
-        } 
-        if(c == '\n'){
+        }
+        if (c == '\n') {
             updates[update_index][page_index] = atoi(input_buffer);
-            updates[update_index][page_index + 1] = -1;//indicate end
+            updates[update_index][page_index + 1] = -1;  // indicate end
             buffer_index = 0;
             page_index = 0;
             update_index++;
             continue;
-        } 
+        }
     }
     update_ct = update_index;
 
     fclose(fp);
 
-    for (int i = 0; i < update_ct; i++){
+    for (int i = 0; i < update_ct; i++) {
         int *update = updates[i];
-        if (is_valid_update(update, pairs, pair_ct)){
+        if (is_valid_update(update, pairs, pair_ct)) {
             part_one_result += get_middle_value(update);
-        }else{
+        } else {
             revise_update(update, pairs, pair_ct);
             part_two_result += get_middle_value(update);
         }
@@ -164,8 +173,8 @@ int main(int argc, char const *argv[]){
 
     printf("Part one total is %u\n", part_one_result);
     printf("Part two total is %u\n", part_two_result);
-    
-    //cleanup
+
+    // cleanup
 
     return 0;
 }
