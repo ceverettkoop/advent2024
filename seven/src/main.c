91ea511a95a38fc991762301e6f6ac0d950774ca
Sean McBride <brennsto@gmail.com>
2025-01-01 16:23:08 -0800

    uhhh some power issues

--- seven/src/main.c
+++ seven/src/main.c
@@ -7,7 +7,7 @@
 
 #include "../../include/error.h"
 
-const char path[] = "./phonyinput";
+const char path[] = "./input";
 
 #define MAX_DIGITS 16
 #define MAX_INPUTS 16
@@ -19,35 +19,62 @@ typedef struct Equation_tag {
         size_t input_ct;
 } Equation;
 
-typedef struct Operation_tag{
-    int a;
-    int b;
-    int operator;
+typedef struct Operation_tag {
+        int a;
+        int b;
+        int operator;
 } Operation;
 
 enum { ADDING = 0, MULTIPLYING = 1 };
 
-static int solve(Operation input){
-    if(input.operator == ADDING){
+static void int_to_bitset(size_t n, int *bitset, size_t bit_ct) {
+    memset(bitset, 0, sizeof(int) * bit_ct);
+    for (size_t i = 0; i < bit_ct; i++) {
+        if (n == 0) break;
+        bitset[i] = n % 2;
+        n = n / 2;
+    }
+}
+
+static int solve(Operation input) {
+    if (input.operator== ADDING) {
         return input.a + input.b;
-    }else{
+    } else {
         return input.a * input.b;
     }
 }
 
+static bool equation_matches(const Equation *eq, int *operators) {
+    int sum = 0;
+
+    for (size_t i = 0; i < (eq->input_ct - 1); i++) {
+        Operation op;
+        op.operator= operators[i];
+        if (i == 0) {
+            op.a = eq->inputs[0];
+            op.b = eq->inputs[1];
+        } else {
+            op.a = sum;
+            op.b = eq->inputs[i + 1];
+        }
+        sum = solve(op);
+    }
+    return sum == eq->result;
+}
+
 static bool can_be_true(const Equation *eq) {
     size_t operator_ct = eq->input_ct - 1;
-    size_t word_size = (operator_ct * operator_ct);
-    size_t combinations = word_size - 1;
+    size_t word_size = operator_ct > 1 ? (1 << operator_ct) : 1;
+    size_t combinations = eq->input_ct == 2 ? 2 : word_size - 1 ;
+    int *operator_types = malloc(sizeof(int) * operator_ct);
 
     for (size_t i = 0; i < combinations; i++) {
-        
-
-
+        int_to_bitset(i, operator_types, operator_ct);
+        if (equation_matches(eq, operator_types)) return true;
     }
 
-
-
+    free(operator_types);
+    return false;
 }
 
 int main(int argc, char const *argv[]) {
@@ -57,7 +84,7 @@ int main(int argc, char const *argv[]) {
     char number_buffer[MAX_DIGITS] = {'\0'};
     size_t buffer_index = 0;
     size_t input_index = 0;
-    unsigned part_one_result = 0;
+    unsigned long part_one_result = 0;
 
     // open input
     FILE *fp = fopen(path, "r");
@@ -98,7 +125,7 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    printf("Part one answer is %u\n", part_one_result);
+    printf("Part one answer is %lu\n", part_one_result);
 
     return 0;
 }
