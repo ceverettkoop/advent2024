e9035ea3c5b79b93c4792325f58a0e4e66b3a21b
Sean <brennsto@gmail.com>
2025-01-10 15:39:32 -0800

    part one done, need to store unique trails

--- ten/src/main.c
+++ ten/src/main.c
@@ -14,7 +14,7 @@
 enum { RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3 };
 
 // globals
-const char path[] = "./phonyinput";
+const char path[] = "./input";
 size_t row_ct = 0;
 size_t col_ct = 0;
 size_t matrix_sz = 0;
@@ -47,11 +47,15 @@ static int get_adj_index(size_t index, int dir, int distance) {
     return new_index;
 }
 
-static void print_puzzle(int *puzzle) {
+static void print_puzzle(int *puzzle, int cursor) {
     for (size_t i = 0; i < matrix_sz; i++) {
-        printf("%d", puzzle[i]);
+        if (cursor == i)
+            printf("X");
+        else
+            printf("%d", puzzle[i]);
         if (((i + 1) % col_ct) == 0) printf("\n");
     }
+    printf("\n");
 }
 
 static int *parse_puzzle() {
@@ -91,14 +95,14 @@ static int *parse_puzzle() {
     return puzzle;
 }
 
-static size_t get_valid_directions(int *puzzle, int pos, bool *valid_dir){
+static size_t get_valid_directions(int *puzzle, int pos, bool *valid_dir) {
     size_t ct = 0;
     memset(valid_dir, false, sizeof(bool) * DIR_COUNT);
-    for (size_t dir = 0; dir < DIR_COUNT; dir++){
+    for (size_t dir = 0; dir < DIR_COUNT; dir++) {
         int adj_pos = get_adj_index(pos, dir, 1);
-        if(adj_pos != OUT_OF_BOUNDS){
-            if((puzzle[adj_pos] - 1) == puzzle[pos]){
-                valid_dir[dir] = true; 
+        if (adj_pos != OUT_OF_BOUNDS) {
+            if ((puzzle[adj_pos] - 1) == puzzle[pos]) {
+                valid_dir[dir] = true;
                 ct++;
             }
         }
@@ -106,62 +110,72 @@ static size_t get_valid_directions(int *puzzle, int pos, bool *valid_dir){
     return ct;
 }
 
-static int dir_for_branch(bool* valid, int branch_index){
+static int dir_for_branch(bool *valid, int branch_index) {
     size_t itr = 0;
-    for (size_t dir = 0; dir < DIR_COUNT; dir++){
-        if(valid[dir]){
-            if(branch_index == itr) return dir;
+    for (size_t dir = 0; dir < DIR_COUNT; dir++) {
+        if (valid[dir]) {
+            if (branch_index == itr) return dir;
             itr++;
         }
     }
     return OUT_OF_BOUNDS;
 }
 
-static void iterate_puzzle(int *puzzle, int pos, bool *reachable_nines, int branch_index){
-    //get new options
+static void iterate_puzzle(int *puzzle, int pos, bool *reachable_nines, int branch_index) {
+    // get new options
     bool valid_directions[DIR_COUNT] = {false};
     size_t branches_open = get_valid_directions(puzzle, pos, valid_directions);
-    if(branches_open == 0) goto SUCCESS_CHECK;
-    //move based on index passed to us from above
-    if(branch_index != INITIAL_RUN){
+    if (branches_open == 0) goto SUCCESS_CHECK;
+    // move based on index passed to us from above
+    if (branch_index != INITIAL_RUN) {
         int dir = dir_for_branch(valid_directions, branch_index);
         pos = get_adj_index(pos, dir, 1);
+        if(pos == OUT_OF_BOUNDS){
+            printf("err\n");
+        }
+        //print_puzzle(puzzle, pos);
+        //after move recalc branches open
+        memset(valid_directions, false, sizeof(bool) * DIR_COUNT);
+        branches_open = get_valid_directions(puzzle, pos, valid_directions);
     SUCCESS_CHECK:
-        if(puzzle[pos] == 9){
+        if (puzzle[pos] == 9) {
             reachable_nines[pos] = true;
-        }   
+            //log the path we have taken here somehow
+        }
     }
-    //pass adj_position down to more branches
-    for (size_t i = 0; i < branches_open; i++){
+    // pass adj_position down to a new branch for each possible direction of the new location
+    for (size_t i = 0; i < branches_open; i++) {
         iterate_puzzle(puzzle, pos, reachable_nines, i);
     }
 }
 
-size_t get_trailhead_score(int *puzzle, int pos){
+size_t get_trailhead_score(int *puzzle, int pos, unsigned *part_two_result) {
     size_t nines_reached = 0;
+    size_t unique_paths_found = 0;
     bool *reachable_nines = malloc(sizeof(bool) * matrix_sz);
     check_malloc(reachable_nines);
     memset(reachable_nines, false, sizeof(bool) * matrix_sz);
-
     iterate_puzzle(puzzle, pos, reachable_nines, INITIAL_RUN);
-    for (size_t i = 0; i < matrix_sz; i++){
-        if(reachable_nines[i]) nines_reached++;
+    for (size_t i = 0; i < matrix_sz; i++) {
+        if (reachable_nines[i]) nines_reached++;
     }
     printf("Score for trailhead at index %d is %zu\n", pos, nines_reached);
     free(reachable_nines);
+    (*part_two_result) += unique_paths_found;
     return nines_reached;
 }
 
 int main(int argc, char const *argv[]) {
     int *puzzle = parse_puzzle();
     unsigned result = 0;
-
-    for (size_t i = 0; i < matrix_sz; i++){
-        if(puzzle[i] == 0){
-            result += get_trailhead_score(puzzle, i);
+    unsigned part_two_result = 0;
+    for (size_t i = 0; i < matrix_sz; i++) {
+        if (puzzle[i] == 0) {
+            result += get_trailhead_score(puzzle, i, &part_two_result);
         }
     }
     printf("Part one answer is %u\n", result);
+    printf("Part two answer is %u\n", part_two_result);
     free(puzzle);
     return 0;
 }
