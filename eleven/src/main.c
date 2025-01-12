232873ca872d9c5a74b52e63399ee88849e7cbaa
Sean McBride <brennsto@gmail.com>
2025-01-12 14:57:50 -0800

    init eleven

---
+++ eleven/src/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+
+#include "../../include/error.h"
+
+int main(int argc, char const *argv[]){
+    /* code */
+    return 0;
+}
+
