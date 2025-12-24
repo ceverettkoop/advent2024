75e0a144a59d58ec66d40d4115ea3d33c94fbe6a
Sean McBride <brennsto@gmail.com>
2025-12-23 22:12:52 -0800

    cleaned up

--- 2025/one/src/main.c
+++ 2025/one/src/main.c
@@ -27,47 +27,46 @@ int main(int argc, char const *argv[]) {
 
     while (c != EOF) {
         c = fgetc(fp);
-        if(c == '\n') goto ROTATE;
-        if(c == 'L') dir_is_left = true;
-        if(c == 'R') dir_is_left = false;
-        if(isnumber(c)){
-            if(num_pos == 4) fatal_err("Input number too many digits\n");
+        if (c == '\n') goto ROTATE;
+        if (c == 'L') dir_is_left = true;
+        if (c == 'R') dir_is_left = false;
+        if (isnumber(c)) {
+            if (num_pos == 4) fatal_err("Input number too many digits\n");
             num_str[num_pos] = c;
             num_pos++;
         }
         continue;
 
-        ROTATE:
-            //get distance and clear number buffer
-            distance = atoi(num_str);
-            num_pos = 0;
-            memset(num_str, '\0', MAX_NUM_LEN + 1);
+    ROTATE:
+        // get distance and clear number buffer
+        distance = atoi(num_str);
+        num_pos = 0;
+        memset(num_str, '\0', MAX_NUM_LEN + 1);
 
-            //accounting for more than a full rotation
-            if(distance > 99){
-                //passing zero each time
-                part_two_ct += distance / 100;
-                distance = distance % 100;
-            }
+        // accounting for more than a full rotation
+        if (distance > 99) {
+            // passing zero each time = tick up part two
+            part_two_ct += distance / 100;
+            distance = distance % 100;
+        }
 
-            //part_two
-            if(dir_is_left) distance = distance * -1;
-            new_pos = pos + distance; //where we would be not accounting for overflow
-            
-            //overflow or underflow, tick up part two if we didn't start or end on 0
-            if(new_pos > 99){
-                new_pos = new_pos - 100;
-                if(new_pos != 0 && pos != 0) part_two_ct++;
-            } 
-            if(new_pos < 0){
-                new_pos = new_pos + 100;   
-                if(new_pos != 0 && pos != 0) part_two_ct++;
-            }
-            pos = new_pos;
-            if(pos == 0){
-                part_one_ct++;
-                part_two_ct++;
-            }
+        if (dir_is_left) distance = distance * -1;
+        new_pos = pos + distance; //position prior to considering overflow
+
+        // overflow or underflow, tick up part two if we didn't start or end on 0
+        if (new_pos > 99) {
+            new_pos = new_pos - 100;
+            if (new_pos != 0 && pos != 0) part_two_ct++;
+        }
+        if (new_pos < 0) {
+            new_pos = new_pos + 100;
+            if (new_pos != 0 && pos != 0) part_two_ct++;
+        }
+        pos = new_pos;
+        if (pos == 0) {
+            part_one_ct++;
+            part_two_ct++;
+        }
     }
 
     printf("Part one password is %zu\n", part_one_ct);
